44b88856f699b3c3577fbcac4db3498e6591f995
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2022-08-23 15:41:06 +0300

    Add files via upload

---
+++ src/Follower.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Follower = ({ avatar_url, html_url, login }) => {
+  return (
+    <article className="card">
+      <img src={avatar_url} />
+      <h4>{login}</h4>
+      <a href={html_url} className="btn">
+        view profile
+      </a>
+    </article>
+  );
+};
+export default Follower;
